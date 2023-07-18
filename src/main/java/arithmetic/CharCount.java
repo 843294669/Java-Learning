d2299c3865d262738d365f71c2ef17095b9f8b6e
独孤九剑 <843294669@qq.com>
2023-07-18 10:01:41 +0800

    字符个数无重复统计。

--- src/main/java/arithmetic/CharCount.java
+++ src/main/java/arithmetic/CharCount.java
@@ -10,7 +10,28 @@ public class CharCount {
         while ((str = br.readLine()) != null) {
             sameCharMaxCount(str);
             diffCharMaxCount(str);
+            nonRepeatCharMaxCount(str);
         }
+    }
+
+    /**
+     *
+     * 计算字符串中含有的不同字符的个数。字符在 ASCII 码范围内( 0~127 ，包括 0 和 127 )，
+     * 换行表示结束符，不算在字符里。不在范围内的不作统计。多个相同的字符只计算一次
+     * 例如，对于字符串 abaca 而言，有 a、b、c 三种不同的字符，因此输出 3 。
+     * @param str
+     */
+    private static void nonRepeatCharMaxCount(String str) {
+        int[] a = new int[128];
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char index = str.charAt(i);
+            if (a[index] == 0) {
+                a[index] = 1;
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 
     /**
