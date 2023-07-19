b6985bc70431709e1c19c069d577f51b0eb079ce
独孤九剑 <843294669@qq.com>
2023-07-19 15:16:16 +0800

    删除无效导入。

--- src/main/java/arithmetic/WildCard.java
+++ src/main/java/arithmetic/WildCard.java
@@ -3,8 +3,6 @@ package arithmetic;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * 通配符
