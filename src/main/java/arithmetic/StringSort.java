7dde51482702692624bbdc3790e72a897f29c6da
独孤九剑 <843294669@qq.com>
2023-07-20 12:25:41 +0800

    字节流逆序输出。

--- src/main/java/arithmetic/StringSort.java
+++ src/main/java/arithmetic/StringSort.java
@@ -2,27 +2,56 @@ package arithmetic;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
-/**
- * 描述
- * 编写一个程序，将输入字符串中的字符按如下规则排序。
- *
- * 规则 1 ：英文字母从 A 到 Z 排列，不区分大小写。
- * 如，输入： Type 输出： epTy
- *
- * 规则 2 ：同一个英文字母的大小写同时存在时，按照输入顺序排列。
- * 如，输入： BabA 输出： aABb
- *
- * 规则 3 ：非英文字母的其它字符保持原来的位置。
- * 如，输入： By?e 输出： Be?y
- *
- * 数据范围：输入的字符串长度满足 1 <= n <= 1000
- *
- */
+
 public class StringSort {
 
     public static void main(String[] args) throws IOException {
+            descSort();
+            // letterSort();
+    }
+
+    /**
+     * 逆序输出输入字符
+     * 由于只是逆序输出，对于输入字符无需转换，使用字节流
+     */
+    private static void descSort() throws IOException {
+        int length;
+        InputStream in = System.in;
+        while (true) {
+            length = in.available();
+            if (length > 1) {
+                byte[] bytes = new byte[length];
+                if (in.read(bytes) != -1) {
+                    byte[] temp = new byte[length];
+                    for (int i = 0; i < length; i++) {
+                        temp[length - i - 1] = bytes[i];
+                    }
+                    System.out.println(new String(temp).trim());
+                }
+            }
+        }
+    }
+
+    /**
+     * 描述
+     * 编写一个程序，将输入字符串中的字符按如下规则排序。
+     *
+     * 规则 1 ：英文字母从 A 到 Z 排列，不区分大小写。
+     * 如，输入： Type 输出： epTy
+     *
+     * 规则 2 ：同一个英文字母的大小写同时存在时，按照输入顺序排列。
+     * 如，输入： BabA 输出： aABb
+     *
+     * 规则 3 ：非英文字母的其它字符保持原来的位置。
+     * 如，输入： By?e 输出： Be?y
+     *
+     * 数据范围：输入的字符串长度满足 1 <= n <= 1000
+     *
+     */
+    private static void letterSort() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String str;
         while ((str = bf.readLine()) != null) {
